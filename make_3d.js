e2a97f5f9bad6a3e86f0068ef7f3c19e04cd134f
mgkim9704 <mgkim9704@kaist.ac.kr>
2016-12-08 11:41:55 +0900

    Update make_3d.js

--- make_3d.js
+++ make_3d.js
@@ -1,23 +1,35 @@
 var width, height, renderer, scene, camera;
 function init() {
-width = window.innerWidth;
-			height = window.innerHeight;
+	width = window.innerWidth;
+	height = window.innerHeight;
 			
-			renderer = new THREE.WebGLRenderer();
-			renderer.setSize(width, height);
-			document.body.appendChild(renderer.domElement);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+	document.body.appendChild(renderer.domElement);
 			
-			scene = new THREE.Scene();
-			
-			var angle=45;
-			var aspect=width/height;
-			var near=0.1;
-			var far=10000;
-			camera = new THREE.PerspectiveCamera(angle, aspect, near, far);
-			camera.position.set(0,0,1000);
-			scene.add(camera);
-			
-			var light = new THREE.PointLight();
-			light.position.set(1000,1000,1000);
-			scene.add(light);
-		}
+	scene = new THREE.Scene();
+		
+	var angle=45;
+	var aspect=width/height;
+	var near=0.1;
+	var far=10000;
+	camera = new THREE.PerspectiveCamera(angle, aspect, near, far);
+	camera.position.set(0,0,1000);
+	scene.add(camera);
+		
+	var light = new THREE.PointLight();
+	light.position.set(1000,1000,1000);
+	scene.add(light);
+}
+
+
+function loop() {
+	list.forEach(function (item){
+		item.rotation.x += 0.02;
+		item.rotation.y += 0.02;
+		item.rotation.z += 0.02;
+	});
+	
+	renderer.render(scene, camera);
+	requestAnimationFrame(loop);
+}
